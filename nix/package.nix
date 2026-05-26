83b3bbd783f48b00ab98ca1057bef46b95a04498
Bastian Stegmann <bastian.stegmann@bearingpoint.com>
2026-05-26 12:37:10 +0200

    fix(nix): correct pnpmDeps hash for fetcherVersion 3

--- nix/package.nix
+++ nix/package.nix
@@ -18,7 +18,7 @@ stdenv.mkDerivation (finalAttrs: {
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 3;
-    hash = "sha256-Ron/OUaETHlIVg9NuZCoDPTgH/dbY1vps2s202l3YI4=";
+    hash = "sha256-hBw6mmprXJNWYsxLnZgG/z4+/+UG99CDobUf97SIngk=";
   };
 
   nativeBuildInputs = [
